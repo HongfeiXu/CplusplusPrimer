31ebddfd187eb689df33c381b4311d1d899170e7
HongfeiXu <icevmj@gmail.com>
2016-04-30 11:18:50 +0800

    as usual

---
+++ ch1/ex1_1.cpp
@@ -0,0 +1,27 @@
+/*
+练习1.1：查阅你使用的编译器文档，确定它所使用的文件命名约定，编译并运行第2页的main程序
+*/
+
+int main ()
+{
+	return 0;
+}
+
+/*
+
+I:\vs\learnCplus\learnCplus>cl /EHsc main.cpp
+用于 x86 的 Microsoft (R) C/C++ 优化编译器 19.00.23918 版
+版权所有(C) Microsoft Corporation。保留所有权利。
+
+main.cpp
+Microsoft (R) Incremental Linker Version 14.00.23918.0
+Copyright (C) Microsoft Corporation.  All rights reserved.
+
+/out:main.exe
+main.obj
+
+I:\vs\learnCplus\learnCplus>main
+
+I:\vs\learnCplus\learnCplus>
+
+*/
