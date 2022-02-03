3d75e34726eb304a693e1305f4c3bdca0a0d60cc
HongfeiXu <icevmj@gmail.com>
2022-02-04 00:32:43 +0800

    make_shared abc

--- project/CplusplusPrimer/src/main.cpp
+++ project/CplusplusPrimer/src/main.cpp
@@ -1,7 +1,11 @@
-﻿#include "Imp\simple_cache.h"
+﻿#include <algorithm>
+#include <vector>
+#include <iostream>
 
 int main()
 {
-	test_simple_cache();
-	return 0;
+	int x;
+	auto c1 = [x](int y) {return x * y > 55; }; // c1是lambda产生的闭包的副本
+	auto c2 = c1; // c2是c1的拷贝
+	auto c3 = c2; // c3是c2的拷贝
 }
